0383f89259e3d8afa4187808e472fc48486af4b1
KSUSISS <mr.bezzliki@mail.ru>
2026-05-22 10:20:49 +0300

    Update IToDoLostManager.cs

--- src/Module.Core/IToDoLostManager.cs
+++ src/Module.Core/IToDoLostManager.cs
@@ -1,1 +1,9 @@
+namespace Lab.Interfaces;
 
+public interface IToDoListManager
+{
+    void AddTask(string description);
+    void RemoveTask(int id);
+    void MarkTaskAsCompleted(int id);
+    string[] GetTasks();
+}
